3157ca26ff488d70b1d1b3110890f0c8a51fb9d9
stshsn <shooooting.star3104@gmail.com>
2021-05-23 17:07:09 +0900

    表記揺れがあったので修正

--- components/BreakingNews.vue
+++ components/BreakingNews.vue
@@ -11,7 +11,7 @@
       </v-col>
       <v-col style="padding-left:0;padding-right:0">
         <div class="BreakingNews-heading-description">
-          {{ $t('福井県庁のサイトへ移動します') }}
+          {{ $t('福井県庁のサイトに移動します') }}
         </div>
       </v-col>
     </v-row>
