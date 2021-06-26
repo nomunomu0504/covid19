6d969d257cdecbd9a6981af0f938cbd5a3cd8151
FooQoo <horseshoes.buzz.17@gmail.com>
2021-06-26 22:02:44 +0900

    axios修正

--- functions/paper.js
+++ functions/paper.js
@@ -3,15 +3,11 @@ const axios = require('axios')
 const apiUrl = process.env.API_URL
 
 exports.handler = async () => {
-  const result = await fetchPaper()
-  return result
-}
-
-async function fetchPaper() {
   const URL = `https://${apiUrl}/api/v1/article/search`
 
   try {
-    return await axios.get(URL, { timeout: 5000 })
+    const res = await axios.get(URL, { timeout: 5000 })
+    return res.data
   } catch (err) {
     return {
       statusCode: 500,
